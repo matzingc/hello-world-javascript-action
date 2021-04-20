650f8ec3a695dd8fc01534ccb2394aaf6424fff3
matzingc <52265748+matzingc@users.noreply.github.com>
2021-04-20 12:18:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 var Git = require("nodegit");
 
-async function run(): Promise<void> {
-  const repoPath = process.env.GITHUB_WORKSPACE || '';
-  core.info(`Checking for changes in '${repoPath}'`);
-  const repo = await Repository.open(repoPath);
-  const status = await repo.getStatus();  
-}
+
+const repoPath = process.env.GITHUB_WORKSPACE || '';
+core.info(`Checking for changes in '${repoPath}'`);
+const repo = await Repository.open(repoPath);
+const status = await repo.getStatus();  
