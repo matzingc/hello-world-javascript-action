0e57fafcdb002f47df35596cd55aa14497c63385
matzingc <52265748+matzingc@users.noreply.github.com>
2021-04-20 12:50:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,35 @@ var Git = require("nodegit");
 nodeGitTests();
 
 async function nodeGitTests() {
-  core.info('running nodeGitTests');
-  const repoPath = process.env.GITHUB_WORKSPACE || '';
-  core.info(`Checking for changes in '${repoPath}'`);
-  const repo = await Git.Repository.open(repoPath);
-  const statusFiles = await repo.getStatus();
-  core.info(`status files count: '${statusFiles.length}'`)
-  for (const file of statusFiles) {
-    core.info(`Status: ${file.status()} - ${file.path()}}`);
-  }
   
-  const index = await repo.refreshIndex();
-  const changes = await index.writeTree();
-  const parent = await repo.getCommit('HEAD');
-  const authorSig = Signature.now(authorName, authorEmail);
-  const committer = Signature.now(authorName, authorEmail);
-  const commitId = await repo.createCommit(
-    'HEAD',
-    authorSig,
-    committer,
-    msg,
-    changes,
-    [parent]
-  );
-  const commit = await repo.getCommit(commitId);
-  core.info(`Commited files. New sha: ${commit.sha()}`);
-  core.setOutput('sha', commit.sha());
+  try {
+    core.info('running nodeGitTests');
+    const repoPath = process.env.GITHUB_WORKSPACE || '';
+    core.info(`Checking for changes in '${repoPath}'`);
+    const repo = await Git.Repository.open(repoPath);
+    const statusFiles = await repo.getStatus();
+    core.info(`status files count: '${statusFiles.length}'`)
+    for (const file of statusFiles) {
+      core.info(`Status: ${file.status()} - ${file.path()}}`);
+    }
+
+    const index = await repo.refreshIndex();
+    const changes = await index.writeTree();
+    const parent = await repo.getCommit('HEAD');
+    const authorSig = Signature.now(authorName, authorEmail);
+    const committer = Signature.now(authorName, authorEmail);
+    const commitId = await repo.createCommit(
+      'HEAD',
+      authorSig,
+      committer,
+      msg,
+      changes,
+      [parent]
+    );
+    const commit = await repo.getCommit(commitId);
+    core.info(`Commited files. New sha: ${commit.sha()}`);
+    core.setOutput('sha', commit.sha());
+  } catch(err) {
+    core.info(err);
+  }
 }
