3e6a61791e7a809529c584761bd30d4e9c89b834
matzingc <52265748+matzingc@users.noreply.github.com>
2021-04-20 12:41:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,31 @@ async function nodeGitTests() {
   for (const file of statusFiles) {
     core.info(`Status: ${file.status()} - ${file.path()}}`);
   }
+  
+  
+  if (statusFiles.length <= 0) {
+    const head = await repo.getHeadCommit();
+    core.setOutput('sha', head.sha());
+  } else {
+    const index = await repo.refreshIndex();
+    const changes = await index.writeTree();
+    const parent = await repo.getCommit('HEAD');
+    const authorSig = Signature.now(authorName, authorEmail);
+    const committer = Signature.now(authorName, authorEmail);
+    const commitId = await repo.createCommit(
+      'HEAD',
+      authorSig,
+      committer,
+      msg,
+      changes,
+      [parent]
+    );
+    const commit = await repo.getCommit(commitId);
+    core.info(`Commited files. New sha: ${commit.sha()}`);
+    core.setOutput('sha', commit.sha());
+
+    core.info('Pushing commit to origin');
+    const remote = await repo.getRemote('origin');
+    await remote.push([`${branch}:${branch}`]);
+  }
 }
